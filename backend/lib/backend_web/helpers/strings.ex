11bdbbddfc7ab1192717f20304211f825cf56869
fabianskier <fabianskier@icloud.com>
2023-01-27 11:12:21 -0300

    CO-0002: Add string helper functions and tests
    Signed-off-by: fabianskier <fabianskier@icloud.com>

---
+++ backend/lib/backend_web/helpers/strings.ex
@@ -0,0 +1,13 @@
+defmodule Backend.Helpers.Strings do
+  def upcase(string) do
+    String.upcase(string)
+  end
+
+  def downcase(string) do
+    String.downcase(string)
+  end
+
+  def capitalize(string) do
+    String.capitalize(string)
+  end
+end
